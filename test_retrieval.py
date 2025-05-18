9fa6d27b01041218bce0630feeefac7f4791bfac
jedick <j3ffdick@gmail.com>
2025-05-18 14:35:40 +0800

    Update retrieval test

--- test_retrieval.py
+++ test_retrieval.py
@@ -2,14 +2,16 @@ import unittest
 import re
 from retrieval import *
 
-pdf_file = "work/Wikipedia.pdf"
+pdf_file = "examples/retrieval/CRISPR.pdf"
 
 
 class TestRetrieval(unittest.TestCase):
 
     def test_retrieve(self):
-        evidence = retrieve_from_pdf(pdf_file, "Wikipedia is an encyclopedia", k=1)
-        self.assertEqual(re.findall("encyclopedia", evidence), ["encyclopedia"])
+        evidence = retrieve_from_pdf(
+            pdf_file, "CRISPR evolution has been described as Lamarckian", k=1
+        )
+        self.assertEqual(re.findall("Koonin", evidence), ["Koonin"])
 
 
 if __name__ == "__main__":
