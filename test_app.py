5c36d8f5a2e983c7a1769a9a5353bff4a47233d2
jedick <j3ffdick@gmail.com>
2025-06-04 12:09:20 +0800

    Update app test

--- test_app.py
+++ test_app.py
@@ -1,15 +1,39 @@
 import unittest
-import pandas
-from app import *
+import subprocess
+import time
+from gradio_client import Client
 
 
 class TestApp(unittest.TestCase):
 
-    def test_prediction_to_df(self):
-        prediction = '{"SUPPORT": 0, "NEI": 0, "REFUTE": 0}'
-        self.assertEqual(
-            type(prediction_to_df(prediction)), pandas.core.frame.DataFrame
+    def setUp(self):
+
+        # Start the Gradio app in a separate process
+        self.process = subprocess.Popen(["python", "app.py"])
+        # Wait for the server to start
+        time.sleep(20)
+
+    def tearDown(self):
+        # Terminate the Gradio process
+        self.process.terminate()
+        self.process.wait()
+
+    def test_app(self):
+        # Setup the Gradio client
+        client = Client("http://127.0.0.1:7860/")
+        # Make a few easy predictions
+        result_support = client.predict(
+            claim="Yes", evidence="Yes", api_name="/query_model"
         )
+        result_nei = client.predict(
+            claim="Yes", evidence="Maybe", api_name="/query_model"
+        )
+        result_refute = client.predict(
+            claim="Yes", evidence="No", api_name="/query_model"
+        )
+        self.assertEqual(result_support["label"], "SUPPORT")
+        self.assertEqual(result_nei["label"], "NEI")
+        self.assertEqual(result_refute["label"], "REFUTE")
 
 
 if __name__ == "__main__":
